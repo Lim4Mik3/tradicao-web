66d9b85ddce6387e68fe956e12e2b8a516e33eb5
Diogo Rocha <rdiogo190@gmail.com>
2025-02-14 12:35:26 -0300

    Update carousel-home.tsx

--- src/pages/Home/slices/carousel-home.tsx
+++ src/pages/Home/slices/carousel-home.tsx
@@ -7,7 +7,7 @@ import { Carousel } from '@/components/Carousel';
 function MultipleItems() {
 
   return (
-    <Carousel className='md:max-w-[1200px] max-w-[320px] m-auto mt-12 flex gap-3 mb-20' dots infinite speed={500} slidesToShow={2} responsive={[{ breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1, } }]} slidesToScroll={1} autoplay>
+    <Carousel className='md:max-w-[1200px] max-w-[320px] m-auto mt-14 flex gap-3 mb-20' dots infinite speed={500} slidesToShow={2} responsive={[{ breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1, } }]} slidesToScroll={1} autoplay>
       <div>
         <img className='rounded-4xl md:w-[550px] md:h-[300px]' src={Banner1} alt="Banner 1" />
       </div>
